0a90e85361beccc56db0dd42a3f2d10441affc3a
benl1 <benl1@live.unc.edu>
2020-02-12 15:22:02 -0500

    implemented basic backtrack search

--- backtrack_assign1.py
+++ backtrack_assign1.py
@@ -1,11 +1,12 @@
 from node import Node
 import argparse
+import sys
 
-class Backtrack():
+class Graph():
     """Use -d flag to load in default values for Graph"""
     def __init__(self, use_default_nodes):
         if (use_default_nodes):
-            self.colors = ['Red', 'Green', 'Blue']
+            self.colors = {'Red', 'Green', 'Blue'}
             self.nodes = {n: Node(n) for n in ['NSW', 'V', 'SA', 'WA', 'NT', 'Q', 'TZ']}
             self.buildNodePairs(['NSW Q', 'NSW V', 'NSW SA', 'V SA', 'Q NT', 'Q SA', 'NT WA', 'NT SA', 'WA SA'])
         else:
@@ -14,7 +15,7 @@ class Backtrack():
         self.printGraph()
     
     def readInputs(self):
-        self.colors = readUntilNewLine()
+        self.colors = set(readUntilNewLine())
         nodes = readUntilNewLine()
         self.nodes = {n: Node(n) for n in nodes}
         nodePairs = readUntilNewLine()
@@ -23,14 +24,13 @@ class Backtrack():
     def buildNodePairs(self, nodePairs):
         for nodePair in nodePairs:
              origin, pair = nodePair.split(" ")
-             if origin not in self.nodes.keys():
-                 print(origin + " is not a valid Node")
-                 break 
-             self.nodes[origin].neighbors.append(pair)
+             #2 way connections
+             self.nodes[origin].neighbors.append(self.nodes[pair])
+             self.nodes[pair].neighbors.append(self.nodes[origin])
 
     def printGraph(self):
         for name, node in self.nodes.items():
-            print(name, node.neighbors)
+            print(name, node.neighbors, node.color)
         
 
         
@@ -48,11 +48,72 @@ def readUntilNewLine():
 
     return all_inputs 
 
+#colors should be a set of all possible colors
+def get_available_colors(node, colors):
+    return list(colors - set([n.color for n in node.neighbors]))
+
+backtrack_steps = 0
+
+def assignColorsRecursive(node, colors):
+    global backtrack_steps
+    #each time this is called, we are searching a node:
+    backtrack_steps += 1
+
+    #first check what colors are available for this node
+    available_colors = get_available_colors(node, colors)
+    
+    #if there are no available colors, this solution is drawing dead
+    if len(available_colors) == 0:
+        return False
+
+    #now for each available color, try using that color for this node
+    for color in available_colors:
+        node.color = color
+        success = True
+
+        #try to recurse on each neighbor with the current node color
+        for neighbor in node.neighbors:
+            if neighbor.color == "":
+                success = assignColorsRecursive(neighbor, colors) and success
+        
+        #if we successfully recursed on each other node, return true as we found a solution for this node
+        if success: 
+            return True
+    
+    #if we made it to here, this means we were unable to find a solution for any of the available colors for this node. Reset the color and report the failure
+    node.color = "" 
+    return False
+
+def backtrack_search(g):
+    success = True
+    #this loop is required because it's possible to have nodes that are not connected to any other nodes. One call will be made per "island" in the graph
+    for name, node in g.nodes.items():
+        if node.color == "":
+            success = assignColorsRecursive(node, g.colors) and success
+    #if success is false here, it means that one of the "islands" in the graph could not be resolved
+    return g, success
+
+    
+
+def verify_graph_colors(g):
+    numSearched = 0
+    print("\nVerifying solution")
+    for name, node in g.nodes.items():
+        numSearched += 1
+        nc = node.color
+        for neighbor in node.neighbors:
+            if neighbor.color == nc:
+                return False
+    print("SEARCHED: " , numSearched)
+    return True
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-d', action='store_true')
-    args = parser.parse_args()
-    backtrack = Backtrack(args.d)
+    fp = sys.argv[1]
+    g = Graph(False)
+    g, success = backtrack_search(g)
+    print("Found solution in ", backtrack_steps, " steps: ", verify_graph_colors(g))
+    g.printGraph()
+    
 
 """
 Recurisvely give nodes in the graph colors by making copies of the nodes 
--- node.py
+++ node.py
@@ -2,4 +2,7 @@ class Node():
     def __init__(self, name):
         self.name = name
         self.neighbors = []
-    
+        self.color = ""
+
+    def __repr__(self):
+        return self.name
